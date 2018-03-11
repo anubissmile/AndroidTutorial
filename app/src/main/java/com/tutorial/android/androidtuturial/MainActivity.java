50dff8bf0481a9f8e3678baf7385dddae95c092c
anubissmile <anubis.smile@gmail.com>
2018-03-12 01:37:30 +0700

    Intent to reach the another activity.

--- app/src/main/java/com/tutorial/android/androidtuturial/MainActivity.java
+++ app/src/main/java/com/tutorial/android/androidtuturial/MainActivity.java
@@ -1,13 +1,76 @@
 package com.tutorial.android.androidtuturial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnAddress, btnResume;
+
+    /**
+     * Logic at on create.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /**
+         * Load resources layout.
+         */
         setContentView(R.layout.activity_main);
+
+        /**
+         * Binding Object.
+         */
+        bindingObj();
+
+        /**
+         * Activity on click address button.
+         */
+        onClickAddressButton();
+
+        /**
+         * Activity on click resume button.
+         */
+        onClickResumeButton();
+    }
+
+    private void onClickResumeButton() {
+        btnResume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * Your code here.
+                 */
+
+                /**
+                 * Call Intent object
+                 */
+                Intent intent = new Intent(MainActivity.this, ResumeActivity.class);
+                startActivity(intent); //Load new Activity
+            }
+        });
+    }
+
+    private void onClickAddressButton() {
+        btnAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * Call Intent object.
+                 */
+                Intent intent = new Intent(MainActivity.this, AddressActivity.class);
+                startActivity(intent); // Load new Activity.
+                finish(); // Close the current activity.
+            }
+        });
+    }
+
+    private void bindingObj() {
+        btnAddress = (Button) findViewById(R.id.btnAddress);
+        btnResume = (Button) findViewById(R.id.btnResume);
     }
 }
---
+++ app/src/main/java/com/tutorial/android/androidtuturial/ResumeActivity.java
@@ -0,0 +1,13 @@
+package com.tutorial.android.androidtuturial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResumeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume);
+    }
+}
